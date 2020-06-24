994baf67e86955a5a4496c6397c901a25917fcc5
Lucas Godoi <godoi.lucas@hotmail.com>
2020-06-24 10:48:46 -0300

    First Commit

---
+++ src/usuarios/estrategias-autenticacao.js
@@ -0,0 +1,39 @@
+const passport                  = require('passport');
+const LocalStrategy             = require('passport-local').Strategy;
+const Usuario                   = require('./usuarios-modelo');
+const { InvalidArggumentError } = require('../erros');
+const bcrypt                    = require('bcrypt');
+
+function verificaUsuario(usuario){
+    if(!usuario){
+        throw new InvalidArggumentError('Usuário não encontrado com o e-mail informado');
+    }
+}
+
+
+async function verificarSenha(senha, senhaHash){
+    const senhaValida = await bcrypt.compare(senha, senhaHash);
+    if(!senhaValida){
+        throw new InvalidArggumentError('E-mail ou senha inválidos');
+    }
+}
+
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'senha',
+            session: false
+        }, async (email, senha, done) => {
+            try {
+                const usuario = await Usuario.buscaPorEmail(email);
+                verificaUsuario(usuario);
+                verificarSenha(senha, usuario.senhaHash);
+
+                done(null, usuario);
+            } catch (error) {
+                done(error);
+            }
+        }
+    )
+);
